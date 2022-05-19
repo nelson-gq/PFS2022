906633a0946615bf7c426740c4579c55f5c8077f
nelson-gq <nequispe20@gmail.com>
2022-05-19 20:09:53 -0300

    master

---
+++ 1-PB/Clase_1/calculoDescuento.ts
@@ -0,0 +1,5 @@
+let precioInicial : number = 450.5;
+let descuento : number = 450.5*0.1;
+let precioFinal : number = precioInicial-descuento;
+
+console.log("El precio final es de $ " + precioFinal );
